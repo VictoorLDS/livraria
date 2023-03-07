1265db2e5c0a0ed4e9b10a9c1cbf488ededb9e90
VictoorLDS <vlds221005@gmail.com>
2023-03-07 16:19:36 -0300

    django

--- livraria/models.py
+++ livraria/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Categoria(models.Model):
-    descricao = models.CharField(max_length=100)
+    nome = models.CharField(max_length=100)
     def __str__(self):
-        return self.descricao
+        return self.nome
     
 class Editora(models.Model):
     nome = models.CharField(max_length=100)
