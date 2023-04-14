c27b05182413c1c7b2706e3f38a829b715a23e71
VictoorLDS <vlds221005@gmail.com>
2023-04-14 16:58:58 -0300

    API autores e livros

--- livraria/serializers.py
+++ livraria/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 
-from livraria.models import Categoria, Editora
+from livraria.models import Autor, Categoria, Editora, Livro
 
 class CategoriaSerializer(ModelSerializer):
     class Meta:
@@ -12,3 +12,19 @@ class EditoraSerializer(ModelSerializer):
     class Meta:
         model = Editora
         fields = "__all__"
+
+class AutorSerializer(ModelSerializer):
+    class Meta:
+        model = Autor
+        fields = "__all__"
+
+class LivroSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = "__all__"
+
+class LivroDetailSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = "__all__"
+        depth = 1
--- livraria/views.py
+++ livraria/views.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 
-from livraria.models import Categoria, Editora
-from livraria.serializers import CategoriaSerializer, EditoraSerializer
+from livraria.models import Autor, Categoria, Editora, Livro
+from livraria.serializers import AutorSerializer, CategoriaSerializer, EditoraSerializer, LivroSerializer, LivroDetailSerializer
 
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
@@ -11,3 +11,15 @@ class CategoriaViewSet(ModelViewSet):
 class EditoraViewSet(ModelViewSet):
     queryset = Editora.objects.all()
     serializer_class = EditoraSerializer
+
+class AutorViewSet(ModelViewSet):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+
+class LivroViewSet(ModelViewSet):
+    queryset = Livro.objects.all()
+    
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return LivroDetailSerializer
+        return LivroSerializer
